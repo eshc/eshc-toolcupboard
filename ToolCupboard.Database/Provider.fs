157a4a684e37863cec32695ba3a843f789a65a48
Rudi Horn <dyn-git@rudi-horn.de>
2019-06-07 14:30:03 +0000

    fixed provider

--- ToolCupboard.Database/Provider.fs
+++ ToolCupboard.Database/Provider.fs
@@ -4,16 +4,22 @@ open FSharp.Data.Sql
 
 let [<Literal>] ResolutionPath = __SOURCE_DIRECTORY__ + "/libs"
 
-type sql = SqlDataProvider<
-    Common.DatabaseProviderTypes.POSTGRESQL,
-    ConnectionString.connString,
-    "ToolcupboardDatabase",
-    ResolutionPath,
-    1000,
-    true,
-    "public, admin, references">
+type Db = 
+    SqlDataProvider<
+        Common.DatabaseProviderTypes.POSTGRESQL,
+        ConnectionString.ConnectionString,
+        "ToolcupboardDatabase",
+        ResolutionPath,
+        1000,
+        true,
+        "public, admin, references">
 
 type Context = Db.dataContext option
 
 type UserCard = Db.dataContext.``public.user_cardsEntity``
 type User = Db.dataContext.``public.usersEntity``
+type Tool = Db.dataContext.``public.toolsEntity``
+type ToolCheckout = Db.dataContext.``public.tool_checkoutEntity``
+type AccessLog = Db.dataContext.``public.access_logEntity``
+
+let getContext ctxt = Option.defaultWith (Db.GetDataContext) ctxt
